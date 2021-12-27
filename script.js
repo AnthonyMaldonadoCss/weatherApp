2559029e4dbb571cd254b47258c3f0c7908f4f87
Anthony Maldonado <anthonysistemas20@gmail.com>
2021-12-27 14:25:12 -0400

    API check

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const contenedorPrincipal = document.querySelector('.contenedor_principal');
+const body = document.querySelector('.cuerpo');
 const caja1 = document.querySelector('.caja_1');
 const caja2 = document.querySelector('.caja_2');
 const caja3 = document.querySelector('.caja_3');
@@ -23,12 +24,12 @@ window.addEventListener('load', function(){
 
             const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`
 
-            console.log(URL)
+            // console.log(URL)
 
         fetch(URL)
             .then( response => {return response.json()})
             .then(data => {
-                console.log(data)
+                // console.log(data)
 
                 //pais
                 let country = data.sys.country
@@ -38,16 +39,54 @@ window.addEventListener('load', function(){
                 let temp = Math.round(data.main.temp)
                 //Ubicacion Proxima
                 let name = data.name
+                //Icono
+                let icon = data.weather[0].main
+                //codigos clima
+                // Thunderstorm - Drizzle - Rain - Snow - Atmosphere - Clear - Clouds
                 
                 let [{main, description}] = data.weather
-                console.log(`Pais: ${country}, Velocidad del viento: ${speed} y ${deg}, Temperatura: ${temp}, Localizacion casi exacta: ${name}, Tipo de cielo: ${main} y ${description}`)
                 
                 temperatureValor.textContent =  `${temp} C`
                 temperatureDescription.textContent = description
 
-                ubicacion.textContent = `Localizacion: ${name}, Pais ${country}`
+                ubicacion.textContent = `${name}, Pais ${country}`
 
                 velocidadViento.textContent = `${speed} M/S`
+
+                console.log(icon)
+
+                //Seleccion del icono
+
+                switch(icon){
+                    case`Thunderstorm`:
+                        iconoAnimado.src = 'animated/thunder.svg'
+                        cuerpo.src = 'img/thunderStorm-min.jpg'
+                        break;
+                    
+                    case`Drizzle`:
+                        iconoAnimado.src = 'animated/rainy-4.svg'
+                        break;
+                    
+                    case`Rain`:
+                        iconoAnimado.src = 'animated/rainy-6.svg'
+                        break; 
+                        
+                    case`Snow`:
+                        iconoAnimado.src = 'animated/snowy-6.svg'
+                        break;
+                    
+                    case`Atmosphere`:
+                        iconoAnimado.src = 'animated/cloudy-day-3.svg'
+                        break;
+                    
+                    case`Clear`:
+                        iconoAnimado.src = 'animated/day.svg'
+                        break;
+                
+                    case `Clouds`:
+                        iconoAnimado.src = 'animated/cloudy.svg'
+                        break;
+                }
             
             })
             .catch( error => {
